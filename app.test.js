44068c31e5d5798a15f5428edcf638e71e2234d0
asscat1337 <ruslan.xaziaxmetov@mail.ru>
2022-09-29 23:08:32 +0500

    add yml

--- app.test.js
+++ app.test.js
@@ -1,12 +1,18 @@
 import supertest from 'supertest'
-import {app} from "./server.js";
+import {server} from './server';
 
-const request = supertest(app)
+const request = supertest(server)
+
 
 describe('/test endpoint',()=>{
-    it('should return response',async()=>{
+    it('should return response',async ()=>{
         const response = await request.get('/test')
         expect(response.status).toBe(200)
         expect(response.text).toBe('Hello World')
+
+    })
+    afterAll(()=>{
+        server.close()
     })
 })
+
--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express()
 app.use('/',router)
 
 
-app.listen(3000,'0.0.0.0',()=>{
+const server = app.listen(3000,'0.0.0.0',()=>{
     console.log('listen in 3000 port')
 })
 
 export {
-    app
+    server
 }
